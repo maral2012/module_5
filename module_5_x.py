1aa8f45c54b12825bfe805ab0c76f935156b4547
Maral <martinovalexei568@mail.com>
2025-01-02 18:26:30 +0300

    ready

---
+++ module_5_x.py
@@ -0,0 +1,9 @@
+roket = ['Тополь', 'Сармат', 'Сатана', 'Орешник']
+
+disstansion = [10000, 12000, 11000, 9000]
+
+roket_dist_zip = zip(roket, disstansion)
+
+roket_dist = dict(roket_dist_zip)
+
+print(roket_dist)
