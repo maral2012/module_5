e89eb06accb31c624f22e189532942d95ceb1628
Maral <martinovalexei568@mail.com>
2025-01-03 00:07:07 +0300

    ready

--- module_5_x.py
+++ module_5_x.py
@@ -1,6 +1,36 @@
-roket = ['Тополь', 'Сармат', 'Сатана', 'Орешник']
-disstansion = [10000, 12000, 11000, 9000]
-roket_dist_zip = zip(roket, disstansion)
-roket_dist = dict(roket_dist_zip)
-print(roket_dist)
+# roket = ['Тополь', 'Сармат', 'Сатана', 'Орешник']
+# disstansion = [10000, 12000, 11000, 9000]
+# roket_dist_zip = zip(roket, disstansion)
+# roket_dist = dict(roket_dist_zip)
+# print(roket_dist)
 
+# def my_fn(a,b):
+#     a = a + 1
+#     c = a + b
+#     return c
+# # res = my_fn(3, 4)
+# # print(res)
+# print(my_fn(3, 4))print
+# print(my_fn(4, 6))
+# print(my_fn(1, 3))
+
+
+# def merge_lists_to_dict(a, b):
+#     # c = zip(a, b)
+#     # c = dict(c)
+#     # return c
+#     return dict(zip(a, b))
+# res = merge_lists_to_dict(['Леха', 'Лена', 'Настя'], [46, 44, 41])
+# print(res)
+
+# def merge_lists_to_dict(a, b):
+#        return dict(zip(a, b))
+# res = merge_lists_to_dict(a = ['Леха', 'Лена', 'Настя'], b =[46, 44, 41])
+# print(res)
+# res_2 = merge_lists_to_dict(['Леха'], b =[46, 44, 41])
+# print(res_2)
+
+# def update_car_info(**car):
+#     car['is_available'] = True
+#     return car
+# print(update_car_info(brand = 'xXx', prise = 1000000))
