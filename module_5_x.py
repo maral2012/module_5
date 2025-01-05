2e9bac7e68d697f51a1cad5a2073434277eb8f82
Maral <martinovalexei568@mail.com>
2025-01-05 16:02:54 +0300

    ready

--- module_5_x.py
+++ module_5_x.py
@@ -322,20 +322,3 @@
 # motorcycles.insert(1, 'ducati')
 # print(motorcycles)
 
-class House:
-    def __init__(self, name, number):
-        self.name = name
-        self.number_of_floors = number
-
-    def go_to(self, new_floor):
-        floor = 0
-        if new_floor < 1 or new_floor > self.number_of_floors:
-            print('Такого экажа не существует')
-        else:
-            for floor in range(new_floor):
-                print(floor + 1)
-h1 = House('ЖК Горский', 18)
-h1.go_to(5)
-
-h2 = House('Домик в деревне', 2)
-h2.go_to(10)
